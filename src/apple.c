41e7f2aac7cc934a632c2070b2992e896eeb5d4c
pepper1240 <kwmr9392@icloud.com>
2020-07-07 11:29:35 +0900

    課題完了

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,31 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int i, sum = 0;
+    for (i = 0; i < n; i++) {
+        sum += (A[i] - 1) / x + 1;
+    }
+    return sum <= k;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    
+    lb = 0;
+    ub = 1000000000;
+    
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) ub = mid;
+        else lb = mid;
+    }
+    
+    printf("%d\n", ub);
+    
+    return 0;
 }
--- src/works.c
+++ src/works.c
@@ -4,14 +4,35 @@ int n;
 int k;
 int A[100000];
 
+int p(int x) {
+    int i, j = 0;
+    for (i = 0; i < k; i++) {
+        int sum = 0;
+        while (sum + A[j] <= x && j < n) {
+            sum += A[j];
+            j++;
+        }
+    }
+    return j == n;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    
+    lb = 0;
+    ub = 1000000000;
+    
+    while (ub - lb > 1) {
+        int mid = (lb + ub) / 2;
+        if (p(mid)) ub = mid;
+        else lb = mid;
+    }
+    
+    printf("%d\n", ub);
+    
+    return 0;
 }
